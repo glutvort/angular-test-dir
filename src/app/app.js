685e0541fa475b99d6ba507d3d10e941a8998c04
glutvort <gennadiyl@ikrok.net>
2014-06-20 18:16:39 +0300

    ticket:1 show user data

---
+++ src/app/app.js
@@ -0,0 +1,11 @@
+function mainController($scope) {
+    $scope.user = {
+        firstName:"Eugene",
+        lastName: "Zakharov",
+        email: "evgeniykpi@gmail.com",
+        jabber: "evgeniy@jabber.me",
+        birthDate: "22.01.1994",
+        skype: "evgeniy_zakharov1",
+        bio: "Something about me"
+    }
+}
