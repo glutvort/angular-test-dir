98e223240118e5484a7cdc0c0fb474c1a5540981
glutvort <gennadiyl@ikrok.net>
2014-06-25 18:37:45 +0300

    ticket:4 filters and paginations

--- django_angular_backend/static/contactsController.js
+++ django_angular_backend/static/contactsController.js
@@ -1,6 +1,19 @@
 function contactsController($scope,$http){
+    $scope.searchValue = ""
+    $scope.orderValue = 'first_name'
+    $scope.reverse = false
+    $scope.options = [
+        {name:"Name", value:"first_name"},
+        {name:"Email", value:"'email'"},
+        {name:"Cell phone", value:"phone_number"},
+        {name:"Landline", value:"cellphone_number"}
+    ]
+    $scope.minPage = 1
+    $scope.paginations = {p:1,n:10}
+    $scope.orderOpt = $scope.options[0]
     $http({method:"GET", url: 'http://127.0.0.1:8000/api/v1/contact'}).success(function(data){
         $scope.contactsModel = data.objects
+        $scope.maxPage = parseInt($scope.contactsModel.length/$scope.paginations.n) + 1
     })
     $scope.openContactDetails = function(contact){
         window.location.href = '#/contacts/'+contact.id
--- django_angular_backend/static/routeProvider.js
+++ django_angular_backend/static/routeProvider.js
@@ -5,4 +5,22 @@ angular.module('app', ['ngRoute'])
             when('/contacts', {templateUrl: 'list.html',   controller: contactsController}).
             when('/contacts/:contactId', {templateUrl: 'contact.html',   controller: singleContactController}).
             otherwise({redirectTo: '/home'});
-    }]);
+    }]).filter('usaNumber', function () {
+        return function (item) {
+            if(item[0]=="+" && item[1]=="1") {
+                item = item + " (USA)"
+            }
+            return item
+        };
+    }).filter('paginationFilter', function () {
+        return function (items,pager) {
+            var result = [];
+
+            angular.forEach(items,function(item,index){
+                if ((index+1) > pager.n*(pager.p-1) && (index+1) <= pager.n*pager.p)
+                    result.push(item);
+            })
+
+            return result;
+        };
+    });
