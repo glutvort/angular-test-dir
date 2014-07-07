9081a4008f24046c89e4c3387744af8d613dd1de
glutvort <gennadiyl@ikrok.net>
2014-07-07 15:49:02 +0300

    ticket:2 made corrections according to comments

--- django_angular_backend/static/services.js
+++ django_angular_backend/static/services.js
@@ -1,14 +1,10 @@
 app.factory('contactsService', ['$http', '$q',
         function ($http, $q) {
-            var svcData = {
-                contacts:{}
-            }
-
 
             var getContacts = function(){
                 var deferred = $q.defer();
 
-                $http({method:"GET", url: 'http://127.0.0.1:8000/api/v1/contact'}).success(function(data){
+                $http({method:"GET", url: location.origin+'/api/v1/contact'}).success(function(data){
                     deferred.resolve(data.objects)
                 }).error(function(){
                     deferred.reject()
@@ -18,7 +14,6 @@ app.factory('contactsService', ['$http', '$q',
 
 
             return  {
-                data: svcData,
                 methods: {
                     getContacts: getContacts
                 }
